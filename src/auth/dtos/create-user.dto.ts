101c62587b844a75905dbd669e46da4ba23bd3ed
= <bekuzarovchermen@gmail.com>
2025-05-01 15:50:13 +0300

    Commit: create dto and entity

---
+++ src/auth/dtos/create-user.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, isString, IsString, Min, MinLength } from "class-validator"
+import { Match } from "src/decorators/match-decorator"
+
+
+export class CreateUserDto{
+    user_id: number
+
+    @MinLength(5, {message: 'too short user name'})
+    @IsString()
+    user_name: string
+
+    @IsEmail({}, {message: 'wrong email'})
+    @IsString()
+    email: string
+
+    @MinLength(6)
+    @IsString()
+    password: string
+
+    @IsString()
+    @Match('password', {message: 'your passwords do not match'})
+    confirm_password: string
+}
