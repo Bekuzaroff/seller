1e4c7955e7582d91e22621f732ebdc9d9e4da310
= <bekuzarovchermen@gmail.com>
2025-05-01 18:22:02 +0300

    Commit: signup: hashing password, creating user adding him to db, giving jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/api/v1/user/')
+export class AuthController {
+    constructor(private readonly auth_service: AuthService){
+
+    }
+    @Post('create')
+    sign_up(@Body() user: CreateUserDto){
+        return this.auth_service.sign_up(user);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly repository: Repository<UserEntity>,
+        private readonly configService: ConfigService
+    ){
+    }
+
+    async sign_jwt(id: number){
+        return jwt.sign({id}, this.configService.get<string>('JWT_SECRET_STR') ?? '', {
+            expiresIn: this.configService.get<number>('JWT_EXPIRES')
+        })
+    }
+
+    async sign_up(user: CreateUserDto){
+        try{
+            const new_user = this.repository.create(user);
+            
+            const salt_rounds = 10;
+            new_user.password = await bcrypt.hash(new_user.password, salt_rounds);
+
+            await this.repository.save(new_user);
+
+            const token = await this.sign_jwt(new_user.user_id);
+
+            return {
+                status: 'success',
+                data: token
+            }
+        }catch(e){
+            if(e.code === '23505'){
+                let unique_field = '';
+
+                if(e.detail.includes('user_name')){
+                    unique_field = "user name";
+                }else{
+                    unique_field = "email";
+                }
+
+                throw new ConflictException(`user with such ${unique_field} already exists`);
+            };
+            throw e;
+        }
+    }
+}
