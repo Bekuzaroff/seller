2958ec22410ff4d1c00e9b381ca8750c303837eb
= <bekuzarovchermen@gmail.com>
2025-05-01 15:06:10 +0300

    Commit: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
+  }), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+      type: "postgres",
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USER_NAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DATA_BASE'),
+      synchronize: true,
+      autoLoadEntities: true
+    })
   })],
   controllers: [AppController],
   providers: [AppService],
