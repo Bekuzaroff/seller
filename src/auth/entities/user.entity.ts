8c06fb66f4a6debf6bd229c4860ab134db1b3dff
= <bekuzarovchermen@gmail.com>
2025-05-01 15:26:17 +0300

    Commit: auth module is ready for features

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity("users")
+export class UserEntity{
+    @PrimaryGeneratedColumn()
+    user_id: number
+
+    @Column({
+        type: 'varchar',
+        length: 100,
+        unique: true
+    })
+    user_name: string
+
+    @Column({
+        type: 'varchar',
+        length: 100,
+        unique: true
+    })
+    email: string
+
+    @Column({
+        type: 'varchar',
+        length: 100
+    })
+    password: string
+}
