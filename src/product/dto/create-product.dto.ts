4f7110608a8a95f442d23debcc18a10abc6954db
= <bekuzarovchermen@gmail.com>
2025-05-14 21:15:55 +0300

    Commit: create create-product.dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import { IsArray, IsBoolean, IsNumber, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+    @IsString({message: 'name should be a string'})
+    @MinLength(3, {message: 'name of your product is too short'})
+    name: string
+
+    @IsString({message: 'description should be a string'})
+    @MinLength(10, {message: 'your description is too short'})
+    description: string
+
+    @IsNumber({}, {message: 'price should be a number'})
+    price: number
+
+    @IsArray({message: 'images should be an array of string'})
+    images: string[]
+
+    @IsBoolean({message: 'is_new should be boolean'})
+    is_new: boolean
+
+    @IsNumber({}, {message: 'user_id should be a number'})
+    user_id: number
+
+}
